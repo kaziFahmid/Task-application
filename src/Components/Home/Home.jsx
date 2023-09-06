4d53e9b93be542947d63e6fed4b0839a150ca76f
unknown <nirjhorislam42@gmail.com>
2023-09-07 02:19:40 +0600

    add task feature added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,64 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+    const [tasks,setTasks]=useState(JSON.parse(localStorage.getItem('tasks')))
+useEffect(()=>{
+    setTasks(JSON.parse(localStorage.getItem('tasks')))
+},[JSON.parse(localStorage.getItem('tasks'))])
   return (
     <>
-    <section className='grid h-96 overflow-y-auto  overflow-x-auto gap-9 max-w-5xl mt-7 mx-auto lg:grid-cols-3 md:grid-cols-2 grid-cols-1'>
-{/* pending*/}
+      <section className="grid h-[480px]   overflow-y-auto  overflow-x-auto gap-9 max-w-5xl mt-7 mx-auto lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
+        {/* pending*/}
         <div>
-    <h1 className='font-semibold text-yellow-500 mb-4 '>Pending</h1>
+          <h1 className="font-semibold text-yellow-500 mb-4 ">Pending</h1>
 
-    <div>
+          <div className="flex justify-center flex-col items-start">
+            {tasks?.map((task)=><div className="card w-full bg-base-100 shadow-xl px-6 py-6 mt-4">
+              <div>
+                <h2 className="card-title flex justify-between items-center">
+                  <span>{task?.title}</span> <button className="btn ">:</button>
+                </h2>
+                <p className="mt-2">
+                 {task?.description}
+                </p>
+                <div className="flex mt-5 justify-between items-center">
+                <div>{task?.assignTask}</div>
+                 
+                  <div>{task?.dueDate}</div>
+                  <div className={task?.priority==='medium'&&'text-yellow-500'|| task?.priority==='high'&&'text-red-500'|| task?.priority==='low'&&'text-red-500'}>{task?.priority}</div>
+                </div>
+              </div>
+            </div>
+)}
+            
+            
+            
+            
+            
+            
+          
 
-    <div className="card  bg-base-100 shadow-xl px-6 py-6">
-      
-  <div >
-    
-    <h2 className="card-title flex justify-between items-center"><span>Card title!</span> <button className='btn'>ads</button></h2>
-    <p className='mt-4'>If a dog chews shoes whose shoes does he choose?</p>
-    <div className='flex mt-5 justify-between items-center'>
-      <div>dsaf</div>
-      <div>dsaf</div>
-    </div>
-  </div>
-</div>
 
 
 
-    </div>
+
+
+
+
+
+
+
+          </div>
         </div>
-{/* In Progress */}
+        {/* In Progress */}
         <div>
-        <h1 className='font-semibold text-yellow-700 mb-4'>In Progress</h1>
-        <div className="card  bg-base-100 shadow-xl">
-  <div className="card-body">
-    <h2 className="card-title">Card title!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
-
+          <h1 className="font-semibold text-yellow-700 mb-4">In Progress</h1>
+          <div className="card  bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Card title!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
         </div>
-{/*  Completed */}
+        {/*  Completed */}
         <div>
-        <h1 className='font-semibold text-green-500 mb-4'> Completed </h1>
-        <div className="card  bg-base-100 shadow-xl">
-  <div className="card-body">
-    <h2 className="card-title">Card title!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
-
+          <h1 className="font-semibold text-green-500 mb-4"> Completed </h1>
+          <div className="card  bg-base-100 shadow-xl">
+            <div className="card-body">
+              <h2 className="card-title">Card title!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
         </div>
-    </section>
-      
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,13 +1,53 @@
-import React from "react";
-import { Link, Outlet, useLoaderData, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, Outlet,  useLocation, useNavigate } from "react-router-dom";
 import { AiFillHome } from "react-icons/ai";
 import { BsPeople } from "react-icons/bs";
 import { AiOutlineDashboard } from "react-icons/ai";
 import useAuth from "../hooks/useAuth/useAuth";
+import Swal from "sweetalert2";
 
 const Main = () => {
-  const {user,logOut}=useAuth()
-  const location=useLocation()
+  const { user, logOut } = useAuth();
+  const location = useLocation();
+  const[userList,setUserList]=useState(JSON.parse(localStorage.getItem('users')))
+const navigate=useNavigate()
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [priority, setPriority] = useState("");
+  const [assignTask, setAssignTask] = useState("");
+
+  let handleSubmit = (e) => {
+
+    e.preventDefault();
+
+    let task={
+      title,
+description,
+dueDate,
+priority,
+assignTask,
+ownerEmail:user?.email,
+status:'pending'
+    }
+
+    const existingTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+
+    existingTasks.push(task);
+  
+   
+    localStorage.setItem("tasks", JSON.stringify(existingTasks));
+
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Task successfully added!',
+      showConfirmButton: false,
+      timer: 1500
+    })
+ 
+  };
   return (
     <>
       <section className="grid lg:grid-cols-12 grid-cols-1">
@@ -22,20 +62,38 @@ const Main = () => {
 
             <ul className="mt-36 ms-10">
               <Link to="/">
-                <li className={`${location.pathname==='/'? 'bg-slate-200 flex place-items-center items-center gap-3  p-2  rounded-s-lg text-blue-500 ':'flex place-items-center items-center gap-3 hover:bg-slate-200 hover:p-2 hover:rounded-s-lg duration-200 cursor-pointer hover:text-blue-500' } `}>
+                <li
+                  className={`${
+                    location.pathname === "/"
+                      ? "bg-slate-200 flex place-items-center items-center gap-3  p-2  rounded-s-lg text-blue-500 "
+                      : "flex place-items-center items-center gap-3 hover:bg-slate-200 hover:p-2 hover:rounded-s-lg duration-200 cursor-pointer hover:text-blue-500"
+                  } `}
+                >
                   <AiFillHome />
                   <span>Home</span>
                 </li>
               </Link>
               <Link to="/createteam">
-                <li className={`${location.pathname==="/createteam"? 'bg-slate-200 flex place-items-center items-center gap-3  p-2  rounded-s-lg text-blue-500 ':'flex place-items-center items-center gap-3 hover:bg-slate-200 hover:p-2 hover:rounded-s-lg duration-200 cursor-pointer hover:text-blue-500' }  mt-4`}>
+                <li
+                  className={`${
+                    location.pathname === "/createteam"
+                      ? "bg-slate-200 flex place-items-center items-center gap-3  p-2  rounded-s-lg text-blue-500 "
+                      : "flex place-items-center items-center gap-3 hover:bg-slate-200 hover:p-2 hover:rounded-s-lg duration-200 cursor-pointer hover:text-blue-500"
+                  }  mt-4`}
+                >
                   <BsPeople />
                   <span>Create Team</span>
                 </li>
               </Link>
 
               <Link to="/dashboard">
-                <li className={`${location.pathname==="/dashboard"? 'bg-slate-200 flex place-items-center items-center gap-3  p-2  rounded-s-lg text-blue-500 ':'flex place-items-center items-center gap-3 hover:bg-slate-200 hover:p-2 hover:rounded-s-lg duration-200 cursor-pointer hover:text-blue-500' } mt-4`}>
+                <li
+                  className={`${
+                    location.pathname === "/dashboard"
+                      ? "bg-slate-200 flex place-items-center items-center gap-3  p-2  rounded-s-lg text-blue-500 "
+                      : "flex place-items-center items-center gap-3 hover:bg-slate-200 hover:p-2 hover:rounded-s-lg duration-200 cursor-pointer hover:text-blue-500"
+                  } mt-4`}
+                >
                   <AiOutlineDashboard />
                   <span>Dashboard</span>
                 </li>
@@ -43,9 +101,117 @@ const Main = () => {
             </ul>
 
             <div className="text-center mt-40">
-              <button className=" btn bg-blue-500 text-white px-9 hover:-translate-y-3 duration-200 shadow-md">
+           {!user?.email?<button
+                className=" btn bg-blue-500 text-white px-9 hover:-translate-y-3 duration-200 shadow-md"
+                onClick={()=>navigate('/login')}
+              >
+                {" "}
                 Add Task +
-              </button>
+              </button>:   <button
+                className=" btn bg-blue-500 text-white px-9 hover:-translate-y-3 duration-200 shadow-md"
+                onClick={() =>
+                  document.getElementById("my_modal_1").showModal()
+                }
+              >
+                {" "}
+                Add Task +
+              </button>}
+              <dialog id="my_modal_1" className="modal">
+                <div className="modal-box text-start">
+                  <h3 className="font-bold text-lg">Add</h3>
+                  <form>
+                    <div className="mb-4">
+                      <label htmlFor="title" className="block text-gray-600">
+                        Title:
+                      </label>
+                      <input
+                        type="text"
+                        id="title"
+                        className="w-full border border-gray-300 rounded px-3 py-2"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label
+                        htmlFor="description"
+                        className="block text-gray-600"
+                      >
+                        Description:
+                      </label>
+                      <textarea
+                        id="description"
+                        className="w-full border border-gray-300 rounded px-3 py-2"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="mb-4">
+                      <label htmlFor="dueDate" className="block text-gray-600">
+                        Due Date:
+                      </label>
+                      <input
+                        type="date"
+                        id="dueDate"
+                        className="w-full border border-gray-300 rounded px-3 py-2"
+                        value={dueDate}
+                        onChange={(e) => setDueDate(e.target.value)}
+                        required
+                      />
+                    </div>
+
+                    <div className="mb-4">
+                      <label
+                        htmlFor="assigntask"
+                        className="block text-gray-600"
+                      >
+                        Assign Task:
+                      </label>
+                      <select
+                        id="assigntask"
+                        className="w-full border border-gray-300 rounded px-3 py-2"
+                        value={assignTask}
+                        onChange={(e) => setAssignTask(e.target.value)}
+                        required
+                      >
+                        {userList.filter((x)=>x?.email!==user?.email).map((x,index)=><option key={index} value={x?.name}>{x?.name}</option>)}
+
+               
+                      </select>
+                    </div>
+
+                    <div className="mb-4">
+                      <label htmlFor="priority" className="block text-gray-600">
+                        Priority:
+                      </label>
+                      <select
+                        id="priority"
+                        className="w-full border border-gray-300 rounded px-3 py-2"
+                        value={priority}
+                        onChange={(e) => setPriority(e.target.value)}
+                        required
+                      >
+                        <option value="low">Low</option>
+                        <option value="medium">Medium</option>
+                        <option value="high">High</option>
+                      </select>
+                    </div>
+                  </form>
+                  <div className="modal-action">
+                    <form method="dialog">
+                      <button
+                        onClick={handleSubmit}
+                        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 btn me-4"
+                      >
+                        Add Task
+                      </button>
+                      <button className="btn">Close</button>
+                    </form>
+                  </div>
+                </div>
+              </dialog>
             </div>
           </div>
         </div>
@@ -53,40 +219,50 @@ const Main = () => {
           <div className="min-h-screen">
             <div className="navbar  pt-6 text-neutral-content items-center flex justify-end">
               <div className="text-black flex gap-6 justify-center items-center">
-             
-               {user?.email&& <div className="avatar">
-                  <div className="w-14 rounded-full">
-                    <img src={user?.photoURL} />
+                {user?.email && (
+                  <div className="avatar">
+                    <div className="w-14 rounded-full">
+                      <img src={user?.photoURL} />
+                    </div>
                   </div>
-                </div>
-}
-               {user?.email&& <div>
-                  <h5 className="font-semibold">{user?.displayName}</h5>
-                 
-                </div>}
+                )}
+                {user?.email && (
+                  <div>
+                    <h5 className="font-semibold">{user?.displayName}</h5>
+                  </div>
+                )}
                 <ul className="flex justify-center items-center gap-5">
-                {user?.email? <li>
-              
-                      <button onClick={logOut} className="btn bg-blue-600 text-white px-5">
+                  {user?.email ? (
+                    <li>
+                      <button
+                        onClick={logOut}
+                        className="btn bg-blue-600 text-white px-5"
+                      >
                         Logout
                       </button>
-                  
-                  </li>: <li>
-                    <Link to="/login">
-                      <button className="btn bg-blue-600 text-white px-5">
-                        Login
-                      </button>
-                    </Link>
-                  </li>} 
-                 {!user?.email&& <li>
-                    <Link to="/signup">
-                      <button className="btn   px-5"> Signup</button>
-                    </Link>
-                  </li>}
+                    </li>
+                  ) : (
+                    <li>
+                      <Link to="/login">
+                        <button className="btn bg-blue-600 text-white px-5">
+                          Login
+                        </button>
+                      </Link>
+                    </li>
+                  )}
+                  {!user?.email && (
+                    <li>
+                      <Link to="/signup">
+                        <button className="btn   px-5"> Signup</button>
+                      </Link>
+                    </li>
+                  )}
                 </ul>
               </div>
             </div>
-            <Outlet />
+
+  <Outlet />
+
           </div>
         </div>
       </section>
